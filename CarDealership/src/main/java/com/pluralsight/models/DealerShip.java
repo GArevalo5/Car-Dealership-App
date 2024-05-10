828dca0f3ff0bd8aabc720f35d660b6ebb771e29
Guy Arevalo <arevaloguy@gmail.com>
2024-05-10 12:02:56 -0700

    Lunch update

--- CarDealership/src/main/java/com/pluralsight/models/DealerShip.java
+++ CarDealership/src/main/java/com/pluralsight/models/DealerShip.java
@@ -1,4 +1,49 @@
 package com.pluralsight.models;
 
+import java.util.ArrayList;
+
 public class DealerShip {
+    private String name;
+    private String address;
+    private String phone;
+    private String inventory;
+
+    public DealerShip(String name, String address, String phone, String inventory) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = inventory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(String inventory) {
+        this.inventory = inventory;
+    }
 }
--- CarDealership/src/main/java/com/pluralsight/models/DealershipFileManager.java
+++ CarDealership/src/main/java/com/pluralsight/models/DealershipFileManager.java
@@ -2,12 +2,23 @@ package com.pluralsight.models;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class DealershipFileManager {
+public void inventoryFileReader{
+        //file reader
+        try (FileReader file = new FileReader("files/inventory.csv")) {
+            Scanner fileScanner = new Scanner(file);
 
-    //file reader
-FileReader fileReader = new FileReader("files/inventory.csv");
 
-    public DealershipFileManager() throws FileNotFoundException {
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
+
 }
